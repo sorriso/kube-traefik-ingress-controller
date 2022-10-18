1ea51fc6f4c01f6cf7ee7425a507250b8052d93f
sorriso <sorriso.tls@gmail.com>
2022-10-18 16:06:49 +0200

    #1

--- 0-install.sh
+++ 0-install.sh
@@ -1,12 +1,12 @@
 start=`date +%s`
 
-nerdctl -n k8s.io pull traefik:v2.8.1
+nerdctl -n k8s.io pull traefik:v2.9.1
 
 # Install Traefik Resource Definitions:
-kubectl apply -f https://raw.githubusercontent.com/traefik/traefik/v2.8/docs/content/reference/dynamic-configuration/kubernetes-crd-definition-v1.yml
+kubectl apply -f https://raw.githubusercontent.com/traefik/traefik/v2.9.1/docs/content/reference/dynamic-configuration/kubernetes-crd-definition-v1.yml
 
 # Install RBAC for Traefik:
-kubectl apply -f https://raw.githubusercontent.com/traefik/traefik/v2.8/docs/content/reference/dynamic-configuration/kubernetes-crd-rbac.yml
+kubectl apply -f https://raw.githubusercontent.com/traefik/traefik/v2.9.1/docs/content/reference/dynamic-configuration/kubernetes-crd-rbac.yml
 
 end=`date +%s`
 
